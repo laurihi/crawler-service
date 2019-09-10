c834834ee847d27e58d3d72a4ca717a37e0f8b47
Lauri Hiltunen <hiltusenlauri@gmail.com>
2019-09-10 21:01:17 +0300

    Eureka crawler, initial commit

---
+++ src/main/java/fi/ambientia/sd/crawler/EurekaCrawlerApplication.java
@@ -0,0 +1,13 @@
+package fi.ambientia.sd.crawler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaCrawlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaCrawlerApplication.class, args);
+	}
+
+}
